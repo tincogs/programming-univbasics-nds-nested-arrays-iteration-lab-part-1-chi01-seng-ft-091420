9f703b7a84920481a616079288c66284738af72a
Tom ODay <tomoday91@gmail.com>
2020-09-06 23:48:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ row_index = 0
 while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do
-    if src[row_index][element_index] % 2 == 0
+    if src[row_index][element_index].even?
       p src[row_index][element_index]
     end
     element_index += 1
